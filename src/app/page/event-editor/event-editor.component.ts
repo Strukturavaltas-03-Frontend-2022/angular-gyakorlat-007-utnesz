d7f7575581a5f8b4ae95a24cc08bff072feb5f30
utnesz <90829509+utnesz@users.noreply.github.com>
2022-11-22 14:29:48 +0100

    Ready

--- src/app/page/event-editor/event-editor.component.ts
+++ src/app/page/event-editor/event-editor.component.ts
@@ -1,30 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm, FormControl } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Event } from 'src/app/model/event';
 import { EventService } from 'src/app/service/event.service';
 
 @Component({
   selector: 'app-event-editor',
   templateUrl: './event-editor.component.html',
-  styleUrls: ['./event-editor.component.scss']
+  styleUrls: ['./event-editor.component.scss'],
 })
 export class EventEditorComponent implements OnInit {
-
-  event: Event = new Event;
+  event: Event = new Event();
 
   constructor(
     private eventService: EventService,
-    private activatedRoute: ActivatedRoute
-  ) { }
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-        this.activatedRoute.params.subscribe((params) =>
-          this.eventService.get(params['id']).subscribe((event) => {
-            this.event = event || new Event();
-          })
-        );
+    this.activatedRoute.params.subscribe((params) =>
+      this.eventService.get(params['id']).subscribe((event) => {
+        this.event = event || new Event();
+      })
+    );
   }
 
+  onUpdate(eventForm: NgForm): void {
+    this.eventService.update(eventForm.value).subscribe((event) => {
+      this.router.navigate(['/']);
+    });
+  }
 }
